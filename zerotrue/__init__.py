c61790aba92967044eb3bbb8959b6a7ef12cbdc4
Артемий Бардиан <artemiy.bardian@mail.ru>
2026-05-16 12:14:19 +0300

    chore: bump version to 1.3.1

--- zerotrue/__init__.py
+++ zerotrue/__init__.py
@@ -4,7 +4,7 @@ from zerotrue.async_client import AsyncZeroTrue
 from zerotrue.client import ZeroTrue
 from zerotrue.exceptions import APIError, AuthenticationError, RateLimitError, ValidationError
 
-__version__ = "1.2.0"
+__version__ = "1.3.1"
 __all__ = [
     "ZeroTrue",
     "AsyncZeroTrue",
